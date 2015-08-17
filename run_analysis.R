66d037b345217d6cf082b871a4e915d943300d9c
Patrick Siu <psiu003@gmail.com>
2015-08-16 21:09:46 -0400

    Merged data completed

--- run_analysis.R
+++ run_analysis.R
@@ -50,16 +50,26 @@ loadRawData <- function() {
 ##############################
 ## 1. Merges the training and the test sets to create one data set.
 loadRawData()
-
+merged_data <- rbind(test_data, train_data)
 
 ##############################
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
 
+
+
 ##############################
 ## 3. Uses descriptive activity names to name the activities in the data set
 
+## This requirement was fulfilled during the data load process in loadRawData() using the following:
+## as.character(activity[test_y$V1])
+## as.character(activity[train_y$V1])
+
 ##############################
 ## 4. Appropriately labels the data set with descriptive variable names. 
+
+## This requirement was fulfilled during the data load process in loadRawData() using the following:
+## colnames(test_data) <- c("Activity","Subject", as.character(features))
+## colnames(train_data) <- c("Activity","Subject", as.character(features))
 
 ##############################
 ## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
